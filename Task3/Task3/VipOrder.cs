65d19d413dd43acab5676897a3689343453f9014
Gregory <gregorymalinkovich@gmail.com>
2024-10-14 20:51:15 +0300

    new task3

---
+++ Task3/Task3/VipOrder.cs
@@ -0,0 +1,19 @@
+﻿namespace New_Project
+{
+    public class VIPOrder : Order
+    {
+        public string Gift { get; set; }
+
+        public VIPOrder(string productName, long phoneNumber, float price, string deliveryAddress, string gift)
+            : base(productName, phoneNumber, price, deliveryAddress)
+        {
+            Gift = gift;
+        }
+
+        public override void DisplayInformation()
+        {
+            base.DisplayInformation();
+            Console.WriteLine($"Gift: {Gift}");
+        }
+    }
+}
