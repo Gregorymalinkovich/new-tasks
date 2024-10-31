009c7c82c696ccecdb69d5d88ce29c43abfdca53
Gregorymalinkovich <gregorymalinkovich@gmail.com>
2024-10-31 22:10:08 +0300

    Update DeliveryService.cs

--- Task5/Task5/DeliveryService.cs
+++ Task5/Task5/DeliveryService.cs
@@ -1,10 +1,4 @@
-﻿using New_Project;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Task5
 {
     public class DeliveryService
