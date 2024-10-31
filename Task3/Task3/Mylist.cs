0d752b27b6137f967d41659ba9fc57c78fbed0dc
Gregorymalinkovich <gregorymalinkovich@gmail.com>
2024-10-31 22:03:30 +0300

    Update Mylist.cs

--- Task3/Task3/Mylist.cs
+++ Task3/Task3/Mylist.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace New_Project
 {
     public class MyList<T> : IEnumerable<T>
